844d50b9a519dfaae3810ea493bd176b2fd09ded
Rima <vanethe@gmail.com>
2017-10-04 19:54:03 +0300

    rima update
    yalla

--- HackathonGame/Assets/Test.cs
+++ HackathonGame/Assets/Test.cs
@@ -6,8 +6,9 @@ public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Debug.Log("Update by Ohoud!");
-	}
+        //  Debug.Log("Update by Ohoud!");
+        Debug.Log("Update by Rima!");
+    }
 	
 	// Update is called once per frame
 	void Update () {
