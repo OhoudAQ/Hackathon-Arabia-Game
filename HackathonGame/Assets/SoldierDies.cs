83831ed22212b22dd5ee693faed0540444c34fa9
Rima <vanethe@gmail.com>
2017-10-06 22:04:15 +0300

    Puzzle_02
    Added bullet sprite
    +
    added invisible colider right after the trap

---
+++ HackathonGame/Assets/SoldierDies.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierDies : MonoBehaviour {
+
+    private Animator m_Anim;            // Reference to the player's animator component.
+
+    // Use this for initialization
+    void Start () {
+        m_Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    //TODO play death animation
+    public void Die()
+    {
+        m_Anim.SetBool("Crouch", true);
+
+    }
+}
---
+++ HackathonGame/Assets/TriggerDeath.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDeath : MonoBehaviour {
+    public GameObject bullet;
+
+    private void Awake()
+    {
+
+        
+
+    }
+    //TODO
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+           // bullet.GetComponent<Animation>().Play("BulletShoot");
+
+        }
+    }
+}
