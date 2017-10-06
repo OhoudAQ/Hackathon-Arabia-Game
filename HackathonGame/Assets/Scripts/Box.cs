e498ddc68db62614bd99d0673863a24bf2f4ead0
Rima <vanethe@gmail.com>
2017-10-06 17:28:42 +0300

    Puzzle_02
    First Version
    +
    Death traps 2d pack

---
+++ HackathonGame/Assets/Scripts/Box.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+	// Use this for initialization
+	void Awake () {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+
+    public void moveBoxLeft()
+    {
+        rb2d.velocity = new Vector3(4f, 0f);
+    }
+
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+
+        if (coll.gameObject.name == "MiddleRowBrownbrick")
+        {
+            rb2d.gravityScale = 15;
+        }
+    }
+}
